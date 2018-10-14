758c3806317aec44098a9f0527903e9d05df8341
AVCretella <cretela1@tcnj.edu>
2018-10-14 17:17:29 -0400

    working with gulp to build react app

---
+++ gulpfile.js
@@ -0,0 +1,52 @@
+
+
+
+
+//grab the dependencies that we need
+var gulp = require('gulp');
+var browserify = require('gulp-browserify');
+var concatCss = require('gulp-concat-css');
+var run = require('gulp-run'); //allows us to run a start command with gulp
+
+var src = './process';
+var app = './app';
+
+gulp.task('js', function() {
+  return gulp.src( src + '/js/render.js' )
+    .pipe(browserify({
+      transform: 'reactify',
+      extensions: 'browserify-css',
+      debug: true
+    }))
+    .on('error', function (err) {
+      console.error('Error!', err.message);
+    })
+    .pipe(gulp.dest(app + '/js'));
+});
+
+gulp.task('html', function() {
+  gulp.src( src + '/**/*.html');
+});
+
+gulp.task('css', function() {
+  gulp.src( src + '/css/*.css')
+  .pipe(concatCss('app.css'))
+  .pipe(gulp.dest(app + '/css'));
+});
+
+gulp.task('fonts', function() {
+    gulp.src('node_modules/bootstrap/dist/fonts/**/*')
+    .pipe(gulp.dest(app + '/fonts'));
+});
+
+gulp.task('watch', ['serve'], function() {
+  gulp.watch( src + '/js/**/*', ['js']);
+  gulp.watch( src + '/css/**/*.css', ['css']);
+  gulp.watch([ app + '/**/*.html'], ['html']);
+});
+
+gulp.task('serve', ['html', 'js', 'css'], function() {
+  run('electron app/main.js').exec();
+});
+
+gulp.task('default', ['watch', 'fonts', 'serve']);
