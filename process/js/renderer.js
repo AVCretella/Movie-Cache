65556bfbd780d49f9f05c1c50da3776730bc0077
AVCretella <cretela1@tcnj.edu>
2018-11-03 23:48:21 -0400

    proof of concept that these should in fact be turned into functions and this tutorial slapped some spaghetti into a text editor and made something

--- process/js/renderer.js
+++ process/js/renderer.js
@@ -178,6 +178,24 @@ ReactDOM.render(
   document.getElementById('movieInfo')
 );
 
+function Clock(props) {
+  return (
+    <div>
+      <h1>Hello, world!</h1>
+      <h2>It is {props.date.toLocaleTimeString()}.</h2>
+    </div>
+  );
+}
+
+function tick() {
+  ReactDOM.render(
+    <Clock date={new Date()} />,
+    document.getElementById('movieList')
+  );
+}
+
+setInterval(tick, 1000);
+
 // $(function(){
 //   $('#movieInfo').append('<h3 class="text-success"> Movie Info Loaded </h3>')
 // })
