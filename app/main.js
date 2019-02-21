60f6f40bf471d5ba5e1a6360f4b4dab70c1f75b5
AVCretella <cretela1@tcnj.edu>
2019-02-20 19:35:42 -0500

    but using the .parse library of csv, instead of trying to install csv-parse directly, we can now find in packaged version

--- app/main.js
+++ app/main.js
@@ -99,15 +99,16 @@ app.on('ready', function(){
     event.returnValue='';
     const {dialog} = require('electron');
     const fs = require('fs');
-    const csv = require('csv-parse');
+    // const csv = require('csv-parse');
+    var parse = require('csv').parse;
 
     var importedList = [];
 
     dialog.showOpenDialog({title: 'Select the movie file that you would like to import'}, (filePath) => {
-      if (filePath !== undefined) {
+      if (filePath != undefined) {
         if (which == 'watchlist') { //just need the movie titles
           fs.createReadStream(filePath[0])
-          .pipe(csv())
+          .pipe(parse())
           .on('data', function(data){
             try { //push the first element into the array
               if ( (data[0] != "") && (!importedList.includes(data[0])) ){ //try to get rid of duplicates
